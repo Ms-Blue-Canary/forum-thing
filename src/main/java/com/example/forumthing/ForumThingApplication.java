bc5e5ca01bdf046ed5ac5a03e23aa3bebac103b7
Nicki Swanson <Blue_Canary@sbcglobal.net>
2018-04-01 13:53:32 -0500

    Test

---
+++ src/main/java/com/example/forumthing/ForumThingApplication.java
@@ -0,0 +1,12 @@
+package com.example.forumthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumThingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumThingApplication.class, args);
+	}
+}
